76ffddbc81966056cf09bfc015ea16156301956e
meeatejada491-ship-it <meeatejada491@gmail.com>
2025-11-24 22:21:37 +0000

    lab2

---
+++ calculations/second.js
@@ -0,0 +1,11 @@
+// Lab Assignment 2.5 - Second JavaScript file
+// Calculate Body Mass Index (BMI)
+const weightKg = 70;
+// example weight in kilograms
+const heightM = 1.75;
+// example height in meters
+const bmi= weightKg/(heightM*heightM);
+console.log("Weight(kg):", weightKg);
+console.log("Height(m):", heightM);
+console.log("BMI=", bmi);
+
---
+++ index.js
@@ -0,0 +1,15 @@
+// Lab Assignment 2.4 - Main JavaScript file
+// 1. Simple Addition
+const a=7;
+const b=4;
+const sum = a+b;
+console.log("1) Sum of a+b=", sum);
+// 2. Area of a rectangle 
+const width = 4;
+const height = 3;
+const area = width * height;
+console.log("2) Area of rectangle=", area);
+// 3.Temperature Conversion: Celsius to Fahrenheit
+const celsius = 25;
+const fahrenheit = celsius*9/5+32;
+console.log("3) Temperature in Fahrenheit=", fahrenheit);
